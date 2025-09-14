917f48d1d836097fbc77b6ace2dd5617d8cb58f2
Arti Wagh <artiwagh04@gmail.com>
2025-09-14 23:47:47 +0530

    Handling select dropdown

---
+++ src/test/java/HandlingDropdowns/SelectDropdowmn.java
@@ -0,0 +1,42 @@
+package HandlingDropdowns;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SelectDropdowmn {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://testautomationpractice.blogspot.com/");
+		
+		WebElement dropdown = driver.findElement(By.xpath("//*[@id=\"country\"]"));
+		
+		Select country = new Select(dropdown); 	 
+		//select option from th edropdown
+		
+		//country.selectByVisibleText("Japan");
+//country.selectByIndex(6);
+		country.selectByValue("japan");
+//		
+		
+		//capture all options available from the dropdown
+		
+		List<WebElement> noOfOptions = country.getOptions();
+		System.out.println(noOfOptions.size());
+		
+		for(WebElement obj:noOfOptions) {
+			obj.getText();
+		}
+		
+		
+	}
+}
